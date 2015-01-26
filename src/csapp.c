cf65ff567c53db75a185e118b4ba0453201f9f31
Jonno Schmidt <jonno@schmidtfam.us>
2015-01-26 14:01:11 -0500

    Added starter code

---
+++ src/csapp.c
@@ -0,0 +1,800 @@
+/* $begin csapp.c */
+#include "csapp.h"
+
+/************************** 
+ * Error-handling functions
+ **************************/
+/* $begin errorfuns */
+/* $begin unixerror */
+void unix_error(char *msg) /* unix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+    exit(0);
+}
+/* $end unixerror */
+
+void log_unix_error(char *msg) /* log unix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(errno));
+}
+
+void posix_error(int code, char *msg) /* posix-style error */
+{
+    fprintf(stderr, "%s: %s\n", msg, strerror(code));
+    exit(0);
+}
+
+void dns_error(char *msg) /* dns-style error */
+{
+    fprintf(stderr, "%s: DNS error %d\n", msg, h_errno);
+    exit(0);
+}
+
+void log_dns_error(char *msg) /* dns-style error */
+{
+    fprintf(stderr, "%s: DNS error %d\n", msg, h_errno);
+}
+
+/* $end errorfuns */
+
+/************************************
+ * Wrappers for Unix signal functions 
+ ***********************************/
+
+/* $begin sigaction */
+handler_t *Signal(int signum, handler_t *handler) 
+{
+    struct sigaction action, old_action;
+
+    action.sa_handler = handler;  
+    sigemptyset(&action.sa_mask); /* block sigs of type being handled */
+    action.sa_flags = SA_RESTART; /* restart syscalls if possible */
+
+    if (sigaction(signum, &action, &old_action) < 0)
+	unix_error("Signal error");
+    return (old_action.sa_handler);
+}
+/* $end sigaction */
+
+void Sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
+{
+    if (sigprocmask(how, set, oldset) < 0)
+	unix_error("Sigprocmask error");
+    return;
+}
+
+void Sigemptyset(sigset_t *set)
+{
+    if (sigemptyset(set) < 0)
+	unix_error("Sigemptyset error");
+    return;
+}
+
+void Sigfillset(sigset_t *set)
+{ 
+    if (sigfillset(set) < 0)
+	unix_error("Sigfillset error");
+    return;
+}
+
+void Sigaddset(sigset_t *set, int signum)
+{
+    if (sigaddset(set, signum) < 0)
+	unix_error("Sigaddset error");
+    return;
+}
+
+void Sigdelset(sigset_t *set, int signum)
+{
+    if (sigdelset(set, signum) < 0)
+	unix_error("Sigdelset error");
+    return;
+}
+
+int Sigismember(const sigset_t *set, int signum)
+{
+    int rc;
+    if ((rc = sigismember(set, signum)) < 0)
+	unix_error("Sigismember error");
+    return rc;
+}
+
+
+/********************************
+ * Wrappers for Unix I/O routines
+ ********************************/
+
+int Open(const char *pathname, int flags, mode_t mode) 
+{
+    int rc;
+
+    if ((rc = open(pathname, flags, mode))  < 0)
+	log_unix_error("Open error");
+    return rc;
+}
+
+ssize_t Read(int fd, void *buf, size_t count) 
+{
+    ssize_t rc;
+
+    if ((rc = read(fd, buf, count)) < 0) 
+	log_unix_error("Read error");
+    return rc;
+}
+
+ssize_t Write(int fd, const void *buf, size_t count) 
+{
+    ssize_t rc;
+    /*    printf("writing to fd %d\n",fd); */
+    if ((rc = write(fd, buf, count)) < 0)
+	log_unix_error("Write error");
+    return rc;
+}
+
+off_t Lseek(int fildes, off_t offset, int whence) 
+{
+    off_t rc;
+
+    if ((rc = lseek(fildes, offset, whence)) < 0)
+	log_unix_error("Lseek error");
+    return rc;
+}
+
+void Close(int fd) 
+{
+    int rc;
+
+    if ((rc = close(fd)) < 0)
+	log_unix_error("Close error");
+}
+
+int Select(int  n, fd_set *readfds, fd_set *writefds,
+	   fd_set *exceptfds, struct timeval *timeout) 
+{
+    int rc;
+
+    if ((rc = select(n, readfds, writefds, exceptfds, timeout)) < 0)
+	log_unix_error("Select error");
+    return rc;
+}
+
+int Dup2(int fd1, int fd2) 
+{
+    int rc;
+
+    if ((rc = dup2(fd1, fd2)) < 0)
+	unix_error("Dup2 error");
+    return rc;
+}
+
+void Stat(const char *filename, struct stat *buf) 
+{
+    if (stat(filename, buf) < 0)
+	unix_error("Stat error");
+}
+
+void Fstat(int fd, struct stat *buf) 
+{
+    if (fstat(fd, buf) < 0)
+	unix_error("Fstat error");
+}
+
+/***************************************************
+ * Wrappers for dynamic storage allocation functions
+ ***************************************************/
+
+void *Malloc(size_t size) 
+{
+    void *p;
+
+    if ((p  = malloc(size)) == NULL)
+	unix_error("Malloc error");
+    return p;
+}
+
+void *Realloc(void *ptr, size_t size) 
+{
+    void *p;
+
+    if ((p  = realloc(ptr, size)) == NULL)
+	unix_error("Realloc error");
+    return p;
+}
+
+void *Calloc(size_t nmemb, size_t size) 
+{
+    void *p;
+
+    if ((p = calloc(nmemb, size)) == NULL)
+	unix_error("Calloc error");
+    return p;
+}
+
+void Free(void *ptr) 
+{
+    free(ptr);
+}
+
+/**************************** 
+ * Sockets interface wrappers
+ ****************************/
+
+int Socket(int domain, int type, int protocol) 
+{
+    int rc;
+
+    if ((rc = socket(domain, type, protocol)) < 0)
+	log_unix_error("Socket error");
+    return rc;
+}
+
+int Setsockopt(int s, int level, int optname, const void *optval, int optlen) 
+{
+    int rc;
+
+    if ((rc = setsockopt(s, level, optname, optval, optlen)) < 0)
+	log_unix_error("Setsockopt error");
+
+    return rc;
+}
+
+int Bind(int sockfd, struct sockaddr *my_addr, int addrlen) 
+{
+    int rc;
+
+    if ((rc = bind(sockfd, my_addr, addrlen)) < 0)
+	log_unix_error("Bind error");
+
+    return rc;
+}
+
+int Listen(int s, int backlog) 
+{
+    int rc;
+
+    if ((rc = listen(s,  backlog)) < 0)
+	log_unix_error("Listen error");
+
+    return rc;
+}
+
+int Accept(int s, struct sockaddr *addr, int *addrlen) 
+{
+    int rc;
+
+    if ((rc = accept(s, addr, addrlen)) < 0)
+	log_unix_error("Accept error");
+    return rc;
+}
+
+int Connect(int sockfd, struct sockaddr *serv_addr, int addrlen) 
+{
+    int rc;
+
+    if ((rc = connect(sockfd, serv_addr, addrlen)) < 0)
+	log_unix_error("Connect error");
+
+    return rc;
+}
+
+/************************
+ * DNS interface wrappers 
+ ***********************/
+
+/* $begin gethostbyname */
+struct hostent *Gethostbyname(const char *name) 
+{
+    struct hostent *p;
+
+    if ((p = gethostbyname(name)) == NULL)
+	log_dns_error("Gethostbyname error");
+    return p;
+}
+/* $end gethostbyname */
+
+struct hostent *Gethostbyaddr(const char *addr, int len, int type) 
+{
+    struct hostent *p;
+
+    if ((p = gethostbyaddr(addr, len, type)) == NULL)
+	log_dns_error("Gethostbyaddr error");
+    return p;
+}
+
+/************************************************
+ * Wrappers for Pthreads thread control functions
+ ************************************************/
+
+void Pthread_create(pthread_t *tidp, pthread_attr_t *attrp, 
+		    void * (*routine)(void *), void *argp) 
+{
+    int rc;
+
+    if ((rc = pthread_create(tidp, attrp, routine, argp)) != 0)
+	posix_error(rc, "Pthread_create error");
+}
+
+void Pthread_cancel(pthread_t tid) {
+    int rc;
+
+    if ((rc = pthread_cancel(tid)) != 0)
+	posix_error(rc, "Pthread_cancel error");
+}
+
+void Pthread_join(pthread_t tid, void **thread_return) {
+    int rc;
+
+    if ((rc = pthread_join(tid, thread_return)) != 0)
+	posix_error(rc, "Pthread_join error");
+}
+
+/* $begin detach */
+void Pthread_detach(pthread_t tid) {
+    int rc;
+
+    if ((rc = pthread_detach(tid)) != 0)
+	posix_error(rc, "Pthread_detach error");
+}
+/* $end detach */
+
+void Pthread_exit(void *retval) {
+    pthread_exit(retval);
+}
+
+pthread_t Pthread_self(void) {
+    return pthread_self();
+}
+ 
+void Pthread_once(pthread_once_t *once_control, void (*init_function)()) {
+    pthread_once(once_control, init_function);
+}
+
+
+/*************************************************************
+ * Wrappers for Pthreads mutex and condition variable functions
+ ************************************************************/
+
+void Pthread_mutex_init(pthread_mutex_t *mutex, pthread_mutexattr_t *attr) 
+{
+    int rc;
+
+    if ((rc = pthread_mutex_init(mutex, attr)) != 0)
+	posix_error(rc, "Pthread_mutex_init error");
+}
+
+/* $begin lock */
+void Pthread_mutex_lock(pthread_mutex_t *mutex) 
+{
+    int rc;
+
+    if ((rc = pthread_mutex_lock(mutex)) != 0)
+	posix_error(rc, "Pthread_mutex_lock error");
+}
+/* $end lock */
+
+void Pthread_mutex_unlock(pthread_mutex_t *mutex) 
+{
+    int rc;
+
+    if ((rc = pthread_mutex_unlock(mutex)) != 0)
+	posix_error(rc, "Pthread_mutex_unlock error");
+}
+
+void Pthread_cond_init(pthread_cond_t *cond, pthread_condattr_t *attr) 
+{
+    int rc;
+
+    if ((rc = pthread_cond_init(cond, attr)) != 0)
+	posix_error(rc, "Pthread_cond_init error");
+}
+
+void Pthread_cond_signal(pthread_cond_t *cond) 
+{
+    int rc;
+
+    if ((rc = pthread_cond_signal(cond)) != 0)
+	posix_error(rc, "Pthread_cond_signal error");
+}
+
+void Pthread_cond_broadcast(pthread_cond_t *cond) 
+{
+    int rc;
+
+    if ((rc = pthread_cond_broadcast(cond)) != 0)
+	posix_error(rc, "Pthread_cond_broadcast error");
+}
+
+void Pthread_cond_wait(pthread_cond_t *cond, pthread_mutex_t *mutex) 
+{
+    int rc;
+
+    if ((rc = pthread_cond_wait(cond, mutex)) != 0)
+	posix_error(rc, "Pthread_cond_wait error");
+}
+
+/* $begin timedwait */
+int Pthread_cond_timedwait(pthread_cond_t *cond, 
+			   pthread_mutex_t *mutex,
+			   struct timespec *abstime) 
+{
+    int rc = pthread_cond_timedwait(cond, mutex, abstime);
+
+    if ((rc != 0) && (rc != ETIMEDOUT))  
+	posix_error(rc, "Pthread_cond_timedwait error");
+    return rc;
+}  
+/* $end timedwait */
+
+/*********************************************************************
+ * The Rio package - robust I/O functions
+ **********************************************************************/
+
+
+/* wrapper for "plain" read function --
+   does nothing more than check for EINTR */
+
+ssize_t rio_readp(int fd, void *ptr, size_t nbytes)
+{
+  int n;
+  while(1){
+    if ((n = read(fd, ptr, nbytes)) < 0) {
+      if (errno == EINTR) continue;
+    }
+    break;
+  }
+  return n;
+}
+
+/* wrapper for "plain" write function --
+   does nothing more than check for EINTR */
+
+ssize_t rio_writep(int fd, void *ptr, size_t nbytes)
+{
+  int n;
+  while(1){
+    if ((n = write(fd, ptr, nbytes)) < 0) {
+      if (errno == EINTR) continue;
+    }
+    break;
+  }
+  return n;
+}
+
+/*
+ * rio_readn - robustly read n bytes (unbuffered)
+ */
+/* $begin rio_readn */
+ssize_t rio_readn(int fd, void *usrbuf, size_t n) 
+{
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+	if ((nread = read(fd, bufp, nleft)) < 0) {
+	    if (errno == EINTR) /* interrupted by sig handler return */
+		nread = 0;      /* and call read() again */
+	    else
+		return -1;      /* errno set by read() */ 
+	} 
+	else if (nread == 0)
+	    break;              /* EOF */
+	nleft -= nread;
+	bufp += nread;
+    }
+    return (n - nleft);         /* return >= 0 */
+}
+/* $end rio_readn */
+
+/*
+ * rio_writen - robustly write n bytes (unbuffered)
+ */
+/* $begin rio_writen */
+ssize_t rio_writen(int fd, void *usrbuf, size_t n) 
+{
+    size_t nleft = n;
+    ssize_t nwritten;
+    char *bufp = usrbuf;
+
+    while (nleft > 0) {
+	if ((nwritten = write(fd, bufp, nleft)) <= 0) {
+	    if (errno == EINTR)  /* interrupted by sig handler return */
+		nwritten = 0;    /* and call write() again */
+	    else
+		return -1;       /* errorno set by write() */
+	}
+	nleft -= nwritten;
+	bufp += nwritten;
+    }
+    return n;
+}
+/* $end rio_writen */
+
+
+/* 
+ * rio_read - This is a wrapper for the Unix read() function that
+ *    transfers min(n, rio_cnt) bytes from an internal buffer to a user
+ *    buffer, where n is the number of bytes requested by the user and
+ *    rio_cnt is the number of unread bytes in the internal buffer. On
+ *    entry, rio_read() refills the internal buffer via a call to
+ *    read() if the internal buffer is empty.
+ */
+/* $begin rio_read */
+static ssize_t rio_read(rio_t *rp, char *usrbuf, size_t n)
+{
+    int cnt;
+
+    while (rp->rio_cnt <= 0) {  /* refill if buf is empty */
+	rp->rio_cnt = read(rp->rio_fd, rp->rio_buf, 
+			   sizeof(rp->rio_buf));
+	if (rp->rio_cnt < 0) {
+	    if (errno != EINTR) /* interrupted by sig handler return */
+		return -1;
+	}
+	else if (rp->rio_cnt == 0)  /* EOF */
+	    return 0;
+	else 
+	    rp->rio_bufptr = rp->rio_buf; /* reset buffer ptr */
+    }
+
+    /* Copy min(n, rp->rio_cnt) bytes from internal buf to user buf */
+    cnt = n;          
+    if (rp->rio_cnt < n)   
+	cnt = rp->rio_cnt;
+    memcpy(usrbuf, rp->rio_bufptr, cnt);
+    rp->rio_bufptr += cnt;
+    rp->rio_cnt -= cnt;
+    return cnt;
+}
+/* $end rio_read */
+
+/*
+ * rio_readinitb - Associate a descriptor with a read buffer and reset buffer
+ */
+/* $begin rio_readinitb */
+void rio_readinitb(rio_t *rp, int fd) 
+{
+    rp->rio_fd = fd;  
+    rp->rio_cnt = 0;  
+    rp->rio_bufptr = rp->rio_buf;
+}
+/* $end rio_readinitb */
+
+/*
+ * rio_readnb - Robustly read n bytes (buffered)
+ */
+/* $begin rio_readnb */
+ssize_t rio_readnb(rio_t *rp, void *usrbuf, size_t n) 
+{
+    size_t nleft = n;
+    ssize_t nread;
+    char *bufp = usrbuf;
+    
+    while (nleft > 0) {
+	if ((nread = rio_read(rp, bufp, nleft)) < 0) {
+	    if (errno == EINTR) /* interrupted by sig handler return */
+		nread = 0;      /* call read() again */
+	    else
+		return -1;      /* errno set by read() */ 
+	} 
+	else if (nread == 0)
+	    break;              /* EOF */
+	nleft -= nread;
+	bufp += nread;
+    }
+    return (n - nleft);         /* return >= 0 */
+}
+/* $end rio_readnb */
+
+/* 
+ * rio_readlineb - robustly read a text line (buffered)
+ */
+/* $begin rio_readlineb */
+ssize_t rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) 
+{
+    int n, rc;
+    char c, *bufp = usrbuf;
+
+    n = 0;
+
+    while (n < maxlen-1){
+	if ((rc = rio_read(rp, &c, 1)) == 1) {
+	  n++;
+	  *bufp++ = c;
+	  if (c == '\n')
+	    break;
+	} else if (rc == 0) {
+	  break;    /* EOF */
+	} else
+	  return -1;	  /* error */
+    }
+    *bufp = 0;
+    return n;
+}
+/* $end rio_readlineb */
+
+/**********************************
+ * Wrappers for robust I/O routines
+ **********************************/
+
+ssize_t Rio_readp(int fd, void *ptr, size_t nbytes) 
+{
+    ssize_t n;
+  
+    if ((n = rio_readp(fd, ptr, nbytes)) < 0) {
+        if (errno != EPIPE)
+	   log_unix_error("Rio_readp error");
+	else {
+//	   printf("Rio_readp: socket has closed with EPIPE\n");
+	   return -1;
+	}   
+    }	   
+	   
+    return n;
+}
+
+size_t Rio_writep(int fd, void *usrbuf, size_t n) 
+{
+    size_t rc;
+
+    if ((rc = rio_writep(fd, usrbuf, n)) != n) {
+        if (errno != EPIPE)
+	   log_unix_error("Rio_writep error");
+	else {
+//	   printf("Rio_writep: socket has closed with EPIPE\n");
+        }
+    }	   
+
+    return rc;
+}
+
+ssize_t Rio_readn(int fd, void *ptr, size_t nbytes) 
+{
+    ssize_t n;
+  
+    if ((n = rio_readn(fd, ptr, nbytes)) < 0) {
+        if (errno != EPIPE)
+	   log_unix_error("Rio_readn error");
+	else {
+//	   printf("Rio_readn: socket has closed with EPIPE\n");
+	   return -1;
+	}   
+    }	   
+	   
+    return n;
+}
+
+size_t Rio_writen(int fd, void *usrbuf, size_t n) 
+{
+    size_t rc;
+
+    if ((rc = rio_writen(fd, usrbuf, n)) != n) {
+        if (errno != EPIPE)
+	   log_unix_error("Rio_writen error");
+	else {
+//	   printf("Rio_writen: socket has closed with EPIPE\n");
+        }
+    }	   
+
+    return rc;
+}
+
+void Rio_readinitb(rio_t *rp, int fd)
+{
+    rio_readinitb(rp, fd);
+} 
+
+ssize_t Rio_readnb(rio_t *rp, void *usrbuf, size_t n) 
+{
+    ssize_t rc;
+
+    if ((rc = rio_readnb(rp, usrbuf, n)) < 0)
+	log_unix_error("Rio_readnb error");
+    return rc;
+}
+
+ssize_t Rio_readlineb(rio_t *rp, void *usrbuf, size_t maxlen) 
+{
+    ssize_t rc;
+
+    if ((rc = rio_readlineb(rp, usrbuf, maxlen)) < 0)
+	log_unix_error("Rio_readlineb error");
+    return rc;
+} 
+
+/******************************** 
+ * Client/server helper functions
+ ********************************/
+/*
+ * open_clientfd - open connection to server at <hostname, port> 
+ *   and return a socket descriptor ready for reading and writing.
+ *   Returns -1 and sets errno on Unix error. 
+ *   Returns -2 and sets h_errno on DNS (gethostbyname) error.
+ */
+/* $begin open_clientfd */
+int open_clientfd(char *hostname, int port) 
+{
+    int clientfd;
+    struct hostent *hp;
+    struct sockaddr_in serveraddr;
+
+    if ((clientfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	return -1; /* check errno for cause of error */
+
+    /* Fill in the server's IP address and port */
+    if ((hp = gethostbyname(hostname)) == NULL)
+	return -2; /* check h_errno for cause of error */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    bcopy((char *)hp->h_addr, 
+	  (char *)&serveraddr.sin_addr.s_addr, hp->h_length);
+    serveraddr.sin_port = htons(port);
+
+    /* Establish a connection with the server */
+    if (connect(clientfd, (SA *) &serveraddr, sizeof(serveraddr)) < 0)
+	return -1;
+    return clientfd;
+}
+/* $end open_clientfd */
+
+/*  
+ * open_listenfd - open and return a listening socket on port
+ *     Returns -1 and sets errno on Unix error.
+ */
+/* $begin open_listenfd */
+int open_listenfd(int port) 
+{
+    int listenfd, optval=1;
+    struct sockaddr_in serveraddr;
+  
+    /* Create a socket descriptor */
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+	return -1;
+ 
+    /* Eliminates "Address already in use" error from bind. */
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, 
+		   (const void *)&optval , sizeof(int)) < 0)
+	return -1;
+
+    /* Listenfd will be an endpoint for all requests to port
+       on any IP address for this host */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET; 
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    serveraddr.sin_port = htons((unsigned short)port); 
+    if (bind(listenfd, (SA *)&serveraddr, sizeof(serveraddr)) < 0)
+	return -1;
+
+    /* Make it a listening socket ready to accept connection requests */
+    if (listen(listenfd, LISTENQ) < 0)
+	return -1;
+    return listenfd;
+}
+/* $end open_listenfd */
+
+/******************************************
+ * Wrappers for the client/server helper routines 
+ ******************************************/
+int Open_clientfd(char *hostname, int port) 
+{
+    int rc;
+
+    if ((rc = open_clientfd(hostname, port)) < 0) {
+	if (rc == -1)
+	    log_unix_error("Open_clientfd Unix error");
+	else        
+	    log_dns_error("Open_clientfd DNS error");
+    }
+    return rc;
+}
+
+int Open_listenfd(int port) 
+{
+    int rc;
+
+    if ((rc = open_listenfd(port)) < 0)
+	log_unix_error("Open_listenfd error");
+    return rc;
+}
+/* $end csapp.c */
+
---
+++ src/proxy.c
@@ -0,0 +1,339 @@
+/*
+ * proxy.c - Web proxy for COMPSCI 512
+ *
+ */
+
+#include <stdio.h>
+#include "csapp.h"
+#include <pthread.h>
+
+#define   FILTER_FILE   "proxy.filter"
+#define   LOG_FILE      "proxy.log"
+#define   DEBUG_FILE	"proxy.debug"
+
+
+/*============================================================
+ * function declarations
+ *============================================================*/
+
+int  find_target_address(char * uri,
+			 char * target_address,
+			 char * path,
+			 int  * port);
+
+
+void  format_log_entry(char * logstring,
+		       int sock,
+		       char * uri,
+		       int size);
+		       
+void *forwarder(void* args);
+void *webTalk(void* args);
+void secureTalk(int clientfd, rio_t client, char *inHost, char *version, int serverPort);
+void ignore();
+
+int debug;
+int proxyPort;
+int debugfd;
+int logfd;
+pthread_mutex_t mutex;
+
+/* main function for the proxy program */
+
+int main(int argc, char *argv[])
+{
+  int count = 0;
+  int listenfd, connfd, clientlen, optval, serverPort, i;
+  struct sockaddr_in clientaddr;
+  struct hostent *hp;
+  char *haddrp;
+  sigset_t sig_pipe; 
+  pthread_t tid;
+  int *args;
+  
+  if (argc < 2) {
+    printf("Usage: ./%s port [debug] [webServerPort]\n", argv[0]);
+    exit(1);
+  }
+  if(argc == 4)
+    serverPort = atoi(argv[3]);
+  else
+    serverPort = 80;
+  
+  Signal(SIGPIPE, ignore);
+  
+  if(sigemptyset(&sig_pipe) || sigaddset(&sig_pipe, SIGPIPE))
+    unix_error("creating sig_pipe set failed");
+  if(sigprocmask(SIG_BLOCK, &sig_pipe, NULL) == -1)
+    unix_error("sigprocmask failed");
+  
+  proxyPort = atoi(argv[1]);
+
+  if(argc > 2)
+    debug = atoi(argv[2]);
+  else
+    debug = 0;
+
+
+  /* start listening on proxy port */
+
+  listenfd = Open_listenfd(proxyPort);
+
+  optval = 1;
+  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void*)&optval, sizeof(int)); 
+  
+  if(debug) debugfd = Open(DEBUG_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0666);
+
+  logfd = Open(LOG_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0666);    
+
+
+  /* if writing to log files, force each thread to grab a lock before writing
+     to the files */
+  
+  pthread_mutex_init(&mutex, NULL);
+  
+  while(1) {
+
+    clientlen = sizeof(clientaddr);
+
+    /* accept a new connection from a client here */
+
+    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+    
+    /* you have to write the code to process this new client request */
+
+    /* create a new thread (or two) to process the new connection */
+
+  }
+  
+  if(debug) Close(debugfd);
+  Close(logfd);
+  pthread_mutex_destroy(&mutex);
+  
+  return 0;
+}
+
+/* a possibly handy function that we provide, fully written */
+
+void parseAddress(char* url, char* host, char** file, int* serverPort)
+{
+	char *point1;
+        char *point2;
+        char *saveptr;
+
+	if(strstr(url, "http://"))
+		url = &(url[7]);
+	*file = strchr(url, '/');
+	
+	strcpy(host, url);
+
+	/* first time strtok_r is called, returns pointer to host */
+	/* strtok_r (and strtok) destroy the string that is tokenized */
+
+	/* get rid of everything after the first / */
+
+	strtok_r(host, "/", &saveptr);
+
+	/* now look to see if we have a colon */
+
+	point1 = strchr(host, ':');
+	if(!point1) {
+		*serverPort = 80;
+		return;
+	}
+	
+	/* we do have a colon, so get the host part out */
+	strtok_r(host, ":", &saveptr);
+
+	/* now get the part after the : */
+	*serverPort = atoi(strtok_r(NULL, "/",&saveptr));
+}
+
+
+
+/* this is the function that I spawn as a thread when a new
+   connection is accepted */
+
+/* you have to write a lot of it */
+
+void *webTalk(void* args)
+{
+  int numBytes, lineNum, serverfd, clientfd, serverPort;
+  int tries;
+  int byteCount = 0;
+  char buf1[MAXLINE], buf2[MAXLINE], buf3[MAXLINE];
+  char host[MAXLINE];
+  char url[MAXLINE], logString[MAXLINE];
+  char *token, *cmd, *version, *file, *saveptr;
+  rio_t server, client;
+  char slash[10];
+  strcpy(slash, "/");
+  
+  clientfd = ((int*)args)[0];
+  serverPort = ((int*)args)[1];
+  free(args);
+  
+  Rio_readinitb(&client, clientfd);
+  
+  // Determine protocol (CONNECT or GET)
+
+  // GET: open connection to webserver (try several times, if necessary)
+
+  /* GET: Transfer first header to webserver */
+    
+  // GET: Transfer remainder of the request
+
+  // GET: now receive the response
+
+  // CONNECT: call a different function, securetalk, for HTTPS
+
+}
+
+
+/* this function handles the two-way encrypted data transferred in
+   an HTTPS connection */
+
+void secureTalk(int clientfd, rio_t client, char *inHost, char *version, int serverPort)
+{
+  int serverfd, numBytes1, numBytes2;
+  int tries;
+  rio_t server;
+  char buf1[MAXLINE], buf2[MAXLINE];
+  pthread_t tid;
+  int *args;
+
+  if (serverPort == proxyPort)
+    serverPort = 443;
+  
+  /* Open connecton to webserver */
+  /* clientfd is browser */
+  /* serverfd is server */
+  
+  
+  /* let the client know we've connected to the server */
+
+  /* spawn a thread to pass bytes from origin server through to client */
+
+  /* now pass bytes from client to server */
+
+}
+
+/* this function is for passing bytes from origin server to client */
+
+void *forwarder(void* args)
+{
+  int numBytes, lineNum, serverfd, clientfd;
+  int byteCount = 0;
+  char buf1[MAXLINE];
+  clientfd = ((int*)args)[0];
+  serverfd = ((int*)args)[1];
+  free(args);
+
+  while(1) {
+    
+    /* serverfd is for talking to the web server */
+    /* clientfd is for talking to the browser */
+    
+  }
+}
+
+
+void ignore()
+{
+	;
+}
+
+
+/*============================================================
+ * url parser:
+ *    find_target_address()
+ *        Given a url, copy the target web server address to
+ *        target_address and the following path to path.
+ *        target_address and path have to be allocated before they 
+ *        are passed in and should be long enough (use MAXLINE to be 
+ *        safe)
+ *
+ *        Return the port number. 0 is returned if there is
+ *        any error in parsing the url.
+ *
+ *============================================================*/
+
+/*find_target_address - find the host name from the uri */
+int  find_target_address(char * uri, char * target_address, char * path,
+                         int  * port)
+
+{
+  //  printf("uri: %s\n",uri);
+  
+
+    if (strncasecmp(uri, "http://", 7) == 0) {
+	char * hostbegin, * hostend, *pathbegin;
+	int    len;
+       
+	/* find the target address */
+	hostbegin = uri+7;
+	hostend = strpbrk(hostbegin, " :/\r\n");
+	if (hostend == NULL){
+	  hostend = hostbegin + strlen(hostbegin);
+	}
+	
+	len = hostend - hostbegin;
+
+	strncpy(target_address, hostbegin, len);
+	target_address[len] = '\0';
+
+	/* find the port number */
+	if (*hostend == ':')   *port = atoi(hostend+1);
+
+	/* find the path */
+
+	pathbegin = strchr(hostbegin, '/');
+
+	if (pathbegin == NULL) {
+	  path[0] = '\0';
+	  
+	}
+	else {
+	  pathbegin++;	
+	  strcpy(path, pathbegin);
+	}
+	return 0;
+    }
+    target_address[0] = '\0';
+    return -1;
+}
+
+
+
+/*============================================================
+ * log utility
+ *    format_log_entry
+ *       Copy the formatted log entry to logstring
+ *============================================================*/
+
+void format_log_entry(char * logstring, int sock, char * uri, int size)
+{
+    time_t  now;
+    char    buffer[MAXLINE];
+    struct  sockaddr_in addr;
+    unsigned  long  host;
+    unsigned  char a, b, c, d;
+    int    len = sizeof(addr);
+
+    now = time(NULL);
+    strftime(buffer, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
+
+    if (getpeername(sock, (struct sockaddr *) & addr, &len)) {
+      /* something went wrong writing log entry */
+      printf("getpeername failed\n");
+      return;
+    }
+
+    host = ntohl(addr.sin_addr.s_addr);
+    a = host >> 24;
+    b = (host >> 16) & 0xff;
+    c = (host >> 8) & 0xff;
+    d = host & 0xff;
+
+    sprintf(logstring, "%s: %d.%d.%d.%d %s %d\n", buffer, a,b,c,d, uri, size);
+}
