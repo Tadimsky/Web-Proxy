e191e36bc4c1e5afb448a95edb6d3a5db1660dcc
Jonno Schmidt <jonathan.schmidt@duke.edu>
2015-01-30 01:16:34 -0500

    Reformatted

--- src/proxy.c
+++ src/proxy.c
@@ -9,27 +9,30 @@
 
 #define   FILTER_FILE   "proxy.filter"
 #define   LOG_FILE      "proxy.log"
-#define   DEBUG_FILE	"proxy.debug"
+#define   DEBUG_FILE    "proxy.debug"
 
 
 /*============================================================
  * function declarations
  *============================================================*/
 
-int  find_target_address(char * uri,
-			 char * target_address,
-			 char * path,
-			 int  * port);
+int find_target_address(char *uri,
+        char *target_address,
+        char *path,
+        int *port);
 
 
-void  format_log_entry(char * logstring,
-		       int sock,
-		       char * uri,
-		       int size);
-		       
-void *forwarder(void* args);
-void *webTalk(void* args);
+void format_log_entry(char *logstring,
+        int sock,
+        char *uri,
+        int size);
+
+void *forwarder(void *args);
+
+void *webTalk(void *args);
+
 void secureTalk(int clientfd, rio_t client, char *inHost, char *version, int serverPort);
+
 void ignore();
 
 int debug;
@@ -40,113 +43,112 @@ pthread_mutex_t mutex;
 
 /* main function for the proxy program */
 
-int main(int argc, char *argv[])
-{
-  int count = 0;
-  int listenfd, connfd, clientlen, optval, serverPort, i;
-  struct sockaddr_in clientaddr;
-  struct hostent *hp;
-  char *haddrp;
-  sigset_t sig_pipe; 
-  pthread_t tid;
-  int *args;
-  
-  if (argc < 2) {
-    printf("Usage: ./%s port [debug] [webServerPort]\n", argv[0]);
-    exit(1);
-  }
-  if(argc == 4)
-    serverPort = atoi(argv[3]);
-  else
-    serverPort = 80;
-  
-  Signal(SIGPIPE, ignore);
-  
-  if(sigemptyset(&sig_pipe) || sigaddset(&sig_pipe, SIGPIPE))
-    unix_error("creating sig_pipe set failed");
-  if(sigprocmask(SIG_BLOCK, &sig_pipe, NULL) == -1)
-    unix_error("sigprocmask failed");
-  
-  proxyPort = atoi(argv[1]);
+int main(int argc, char *argv[]) {
+    int count = 0;
+    int listenfd, connfd, clientlen, optval, serverPort, i;
+    struct sockaddr_in clientaddr;
+    struct hostent *hp;
+    char *haddrp;
+    sigset_t sig_pipe;
+    pthread_t tid;
+    int *args;
 
-  if(argc > 2)
-    debug = atoi(argv[2]);
-  else
-    debug = 0;
+    if (argc < 2) {
+        printf("Usage: ./%s port [debug] [webServerPort]\n", argv[0]);
+        exit(1);
+    }
+    if (argc == 4)
+        serverPort = atoi(argv[3]);
+    else
+        serverPort = 80;
 
+    Signal(SIGPIPE, ignore);
 
-  /* start listening on proxy port */
+    if (sigemptyset(&sig_pipe) || sigaddset(&sig_pipe, SIGPIPE))
+        unix_error("creating sig_pipe set failed");
+    if (sigprocmask(SIG_BLOCK, &sig_pipe, NULL) == -1)
+        unix_error("sigprocmask failed");
 
-  listenfd = Open_listenfd(proxyPort);
+    proxyPort = atoi(argv[1]);
 
-  optval = 1;
-  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void*)&optval, sizeof(int)); 
-  
-  if(debug) debugfd = Open(DEBUG_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0666);
-
-  logfd = Open(LOG_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0666);    
+    if (argc > 2)
+        debug = atoi(argv[2]);
+    else
+        debug = 0;
 
 
-  /* if writing to log files, force each thread to grab a lock before writing
-     to the files */
-  
-  pthread_mutex_init(&mutex, NULL);
-  
-  while(1) {
+    /* start listening on proxy port */
 
-    clientlen = sizeof(clientaddr);
+    listenfd = Open_listenfd(proxyPort);
 
-    /* accept a new connection from a client here */
+    optval = 1;
+    setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, (const void *) &optval, sizeof(int));
 
-    connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
-    
-    /* you have to write the code to process this new client request */
+    if (debug) debugfd = Open(DEBUG_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0666);
 
-    /* create a new thread (or two) to process the new connection */
+    logfd = Open(LOG_FILE, O_CREAT | O_TRUNC | O_WRONLY, 0666);
 
-  }
-  
-  if(debug) Close(debugfd);
-  Close(logfd);
-  pthread_mutex_destroy(&mutex);
-  
-  return 0;
+
+    /* if writing to log files, force each thread to grab a lock before writing
+       to the files */
+
+    pthread_mutex_init(&mutex, NULL);
+
+    while (1) {
+
+        clientlen = sizeof(clientaddr);
+
+        /* accept a new connection from a client here */
+        connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen);
+
+
+
+        /* you have to write the code to process this new client request */
+
+        /* create a new thread (or two) to process the new connection */
+
+    }
+
+    if (debug) Close(debugfd);
+    Close(logfd);
+    pthread_mutex_destroy(&mutex);
+
+    return 0;
 }
 
 /* a possibly handy function that we provide, fully written */
 
-void parseAddress(char* url, char* host, char** file, int* serverPort)
-{
-	char *point1;
-        char *point2;
-        char *saveptr;
+void parseAddress(char *url, char *host, char **file, int *serverPort) {
+    char *point1;
+    char *point2;
+    char *saveptr;
 
-	if(strstr(url, "http://"))
-		url = &(url[7]);
-	*file = strchr(url, '/');
-	
-	strcpy(host, url);
+    if (strstr(url, "http://"))
+        url = &(url[7]);
+    *file = strchr(url, '/');
 
-	/* first time strtok_r is called, returns pointer to host */
-	/* strtok_r (and strtok) destroy the string that is tokenized */
+    strcpy(host, url);
 
-	/* get rid of everything after the first / */
+    /* first time strtok_r is called, returns pointer to host */
+    /* strtok_r (and strtok) destroy the string that is tokenized */
 
-	strtok_r(host, "/", &saveptr);
+    /* get rid of everything after the first / */
 
-	/* now look to see if we have a colon */
+    strtok_r(host, "/", &saveptr);
 
-	point1 = strchr(host, ':');
-	if(!point1) {
-		*serverPort = 80;
-		return;
-	}
-	
-	/* we do have a colon, so get the host part out */
-	strtok_r(host, ":", &saveptr);
+    /* now look to see if we have a colon */
 
-	/* now get the part after the : */
-	*serverPort = atoi(strtok_r(NULL, "/",&saveptr));
+    point1 = strchr(host, ':');
+    if (!point1) {
+        *serverPort = 80;
+        return;
+    }
+
+    /* we do have a colon, so get the host part out */
+    strtok_r(host, ":", &saveptr);
+
+    /* now get the part after the : */
+    *serverPort = atoi(strtok_r(NULL, "/", &saveptr));
 }
 
 
@@ -156,36 +158,35 @@ void parseAddress(char* url, char* host, char** file, int* serverPort)
 
 /* you have to write a lot of it */
 
-void *webTalk(void* args)
-{
-  int numBytes, lineNum, serverfd, clientfd, serverPort;
-  int tries;
-  int byteCount = 0;
-  char buf1[MAXLINE], buf2[MAXLINE], buf3[MAXLINE];
-  char host[MAXLINE];
-  char url[MAXLINE], logString[MAXLINE];
-  char *token, *cmd, *version, *file, *saveptr;
-  rio_t server, client;
-  char slash[10];
-  strcpy(slash, "/");
-  
-  clientfd = ((int*)args)[0];
-  serverPort = ((int*)args)[1];
-  free(args);
-  
-  Rio_readinitb(&client, clientfd);
-  
-  // Determine protocol (CONNECT or GET)
+void *webTalk(void *args) {
+    int numBytes, lineNum, serverfd, clientfd, serverPort;
+    int tries;
+    int byteCount = 0;
+    char buf1[MAXLINE], buf2[MAXLINE], buf3[MAXLINE];
+    char host[MAXLINE];
+    char url[MAXLINE], logString[MAXLINE];
+    char *token, *cmd, *version, *file, *saveptr;
+    rio_t server, client;
+    char slash[10];
+    strcpy(slash, "/");
 
-  // GET: open connection to webserver (try several times, if necessary)
+    clientfd = ((int *) args)[0];
+    serverPort = ((int *) args)[1];
+    free(args);
 
-  /* GET: Transfer first header to webserver */
-    
-  // GET: Transfer remainder of the request
+    Rio_readinitb(&client, clientfd);
 
-  // GET: now receive the response
+    // Determine protocol (CONNECT or GET)
 
-  // CONNECT: call a different function, securetalk, for HTTPS
+    // GET: open connection to webserver (try several times, if necessary)
+
+    /* GET: Transfer first header to webserver */
+
+    // GET: Transfer remainder of the request
+
+    // GET: now receive the response
+
+    // CONNECT: call a different function, securetalk, for HTTPS
 
 }
 
@@ -193,54 +194,51 @@ void *webTalk(void* args)
 /* this function handles the two-way encrypted data transferred in
    an HTTPS connection */
 
-void secureTalk(int clientfd, rio_t client, char *inHost, char *version, int serverPort)
-{
-  int serverfd, numBytes1, numBytes2;
-  int tries;
-  rio_t server;
-  char buf1[MAXLINE], buf2[MAXLINE];
-  pthread_t tid;
-  int *args;
+void secureTalk(int clientfd, rio_t client, char *inHost, char *version, int serverPort) {
+    int serverfd, numBytes1, numBytes2;
+    int tries;
+    rio_t server;
+    char buf1[MAXLINE], buf2[MAXLINE];
+    pthread_t tid;
+    int *args;
 
-  if (serverPort == proxyPort)
-    serverPort = 443;
-  
-  /* Open connecton to webserver */
-  /* clientfd is browser */
-  /* serverfd is server */
-  
-  
-  /* let the client know we've connected to the server */
+    if (serverPort == proxyPort)
+        serverPort = 443;
 
-  /* spawn a thread to pass bytes from origin server through to client */
+    /* Open connecton to webserver */
+    /* clientfd is browser */
+    /* serverfd is server */
 
-  /* now pass bytes from client to server */
+
+    /* let the client know we've connected to the server */
+
+    /* spawn a thread to pass bytes from origin server through to client */
+
+    /* now pass bytes from client to server */
 
 }
 
 /* this function is for passing bytes from origin server to client */
 
-void *forwarder(void* args)
-{
-  int numBytes, lineNum, serverfd, clientfd;
-  int byteCount = 0;
-  char buf1[MAXLINE];
-  clientfd = ((int*)args)[0];
-  serverfd = ((int*)args)[1];
-  free(args);
+void *forwarder(void *args) {
+    int numBytes, lineNum, serverfd, clientfd;
+    int byteCount = 0;
+    char buf1[MAXLINE];
+    clientfd = ((int *) args)[0];
+    serverfd = ((int *) args)[1];
+    free(args);
 
-  while(1) {
-    
-    /* serverfd is for talking to the web server */
-    /* clientfd is for talking to the browser */
-    
-  }
+    while (1) {
+
+        /* serverfd is for talking to the web server */
+        /* clientfd is for talking to the browser */
+
+    }
 }
 
 
-void ignore()
-{
-	;
+void ignore() {
+    ;
 }
 
 
@@ -259,50 +257,47 @@ void ignore()
  *============================================================*/
 
 /*find_target_address - find the host name from the uri */
-int  find_target_address(char * uri, char * target_address, char * path,
-                         int  * port)
+int find_target_address(char *uri, char *target_address, char *path,
+        int *port) {
+    //  printf("uri: %s\n",uri);
 
-{
-  //  printf("uri: %s\n",uri);
-  
 
     if (strncasecmp(uri, "http://", 7) == 0) {
-	char * hostbegin, * hostend, *pathbegin;
-	int    len;
-       
-	/* find the target address */
-	hostbegin = uri+7;
-	hostend = strpbrk(hostbegin, " :/\r\n");
-	if (hostend == NULL){
-	  hostend = hostbegin + strlen(hostbegin);
-	}
-	
-	len = hostend - hostbegin;
+        char *hostbegin, *hostend, *pathbegin;
+        int len;
 
-	strncpy(target_address, hostbegin, len);
-	target_address[len] = '\0';
+        /* find the target address */
+        hostbegin = uri + 7;
+        hostend = strpbrk(hostbegin, " :/\r\n");
+        if (hostend == NULL) {
+            hostend = hostbegin + strlen(hostbegin);
+        }
 
-	/* find the port number */
-	if (*hostend == ':')   *port = atoi(hostend+1);
+        len = hostend - hostbegin;
 
-	/* find the path */
+        strncpy(target_address, hostbegin, len);
+        target_address[len] = '\0';
 
-	pathbegin = strchr(hostbegin, '/');
+        /* find the port number */
+        if (*hostend == ':') *port = atoi(hostend + 1);
 
-	if (pathbegin == NULL) {
-	  path[0] = '\0';
-	  
-	}
-	else {
-	  pathbegin++;	
-	  strcpy(path, pathbegin);
-	}
-	return 0;
+        /* find the path */
+
+        pathbegin = strchr(hostbegin, '/');
+
+        if (pathbegin == NULL) {
+            path[0] = '\0';
+
+        }
+        else {
+            pathbegin++;
+            strcpy(path, pathbegin);
+        }
+        return 0;
     }
     target_address[0] = '\0';
     return -1;
 }
-
 
 
 /*============================================================
@@ -311,22 +306,21 @@ int  find_target_address(char * uri, char * target_address, char * path,
  *       Copy the formatted log entry to logstring
  *============================================================*/
 
-void format_log_entry(char * logstring, int sock, char * uri, int size)
-{
-    time_t  now;
-    char    buffer[MAXLINE];
-    struct  sockaddr_in addr;
-    unsigned  long  host;
-    unsigned  char a, b, c, d;
-    int    len = sizeof(addr);
+void format_log_entry(char *logstring, int sock, char *uri, int size) {
+    time_t now;
+    char buffer[MAXLINE];
+    struct sockaddr_in addr;
+    unsigned long host;
+    unsigned char a, b, c, d;
+    int len = sizeof(addr);
 
     now = time(NULL);
     strftime(buffer, MAXLINE, "%a %d %b %Y %H:%M:%S %Z", localtime(&now));
 
-    if (getpeername(sock, (struct sockaddr *) & addr, &len)) {
-      /* something went wrong writing log entry */
-      printf("getpeername failed\n");
-      return;
+    if (getpeername(sock, (struct sockaddr *) &addr, &len)) {
+        /* something went wrong writing log entry */
+        printf("getpeername failed\n");
+        return;
     }
 
     host = ntohl(addr.sin_addr.s_addr);
@@ -335,5 +329,5 @@ void format_log_entry(char * logstring, int sock, char * uri, int size)
     c = (host >> 8) & 0xff;
     d = host & 0xff;
 
-    sprintf(logstring, "%s: %d.%d.%d.%d %s %d\n", buffer, a,b,c,d, uri, size);
+    sprintf(logstring, "%s: %d.%d.%d.%d %s %d\n", buffer, a, b, c, d, uri, size);
 }
